80c03362bb26a4cec97a7ded530b67ba5f0350a6
Matheus Barbosa <matheus.bsa@hotmail.com>
2023-09-30 15:31:24 -0300

    Adiciona funcionalidade para editar informações no modal de informações do livro

--- frontend/src/components/BookInfoModal.vue
+++ frontend/src/components/BookInfoModal.vue
@@ -1,22 +1,58 @@
 <template>
-  <v-dialog v-model="internalDialog" max-width="500" @input="closeDialog">
+  <v-dialog v-model="internalDialog" max-width="500">
     <v-card>
-      <v-card-title class="headline">{{ book.titulo }}</v-card-title>
+      <v-card-title class="headline">{{
+        editing ? "Editar Livro" : "Detalhes do Livro"
+      }}</v-card-title>
 
       <v-card-text>
         <v-container>
           <v-row>
             <v-col>
-              <p><strong>Editora:</strong> {{ book.editora }}</p>
-              <p><strong>Preço:</strong> {{ book.preco }}</p>
-              <p><strong>Páginas:</strong> {{ book.paginas }}</p>
+              <v-text-field
+                v-if="editing"
+                v-model="editedBook.titulo"
+                label="Título"
+              ></v-text-field>
+              <v-row>
+                <v-col>
+                  <v-subheader>Editora:</v-subheader>
+                  <span v-if="!editing">{{ book.editora }}</span>
+                  <v-text-field
+                    v-else
+                    v-model="editedBook.editora"
+                    label="Editora"
+                  ></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-subheader>Preço:</v-subheader>
+                  <span v-if="!editing">{{ book.preco }}</span>
+                  <v-text-field
+                    v-else
+                    v-model="editedBook.preco"
+                    label="Preço"
+                  ></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-subheader>Páginas:</v-subheader>
+                  <span v-if="!editing">{{ book.paginas }}</span>
+                  <v-text-field
+                    v-else
+                    v-model="editedBook.paginas"
+                    label="Páginas"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
 
       <v-card-actions>
-        <v-btn color="blue darken-1" text @click="closeDialog">Fechar</v-btn>
+        <v-btn color="blue darken-1" text @click="closeDialog">Cancelar</v-btn>
+        <v-btn color="blue darken-1" text @click="toggleEditing">{{
+          editing ? "Salvar" : "Editar"
+        }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -34,6 +70,12 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      editedBook: { ...this.book },
+      editing: false,
+    };
+  },
   computed: {
     internalDialog: {
       get() {
@@ -42,7 +84,7 @@ export default {
       set(value) {
         this.$emit("update:dialog", value);
         if (!value) {
-          this.$emit("dialog-closed");
+          this.resetEditedBook();
         }
       },
     },
@@ -50,6 +92,14 @@ export default {
   methods: {
     closeDialog() {
       this.internalDialog = false;
+      this.resetEditedBook();
+    },
+    resetEditedBook() {
+      this.editedBook = { ...this.book };
+      this.editing = false;
+    },
+    toggleEditing() {
+      this.editing = !this.editing;
     },
   },
 };
